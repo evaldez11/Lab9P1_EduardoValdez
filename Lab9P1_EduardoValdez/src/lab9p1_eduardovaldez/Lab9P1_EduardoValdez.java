981b4054755cf7cbb8680a58297ebeadbbeb47ca
evaldez11 <147879061+evaldez11@users.noreply.github.com>
2023-12-08 19:39:17 -0600

    Add files via upload

---
+++ Lab9P1_EduardoValdez/src/lab9p1_eduardovaldez/Lab9P1_EduardoValdez.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+//fila 2, Asiento 1
+package lab9p1_eduardovaldez;
+import java.util.Scanner;
+import java.util.Random;
+
+/**
+ *
+ * @author guali
+ */
+
+public class Lab9P1_EduardoValdez {
+
+    /**
+     * @param args the command line arguments
+     */
+    static Scanner leer = new Scanner(System.in);
+    static Random rand = new Random();
+    public static void main(String[] args) {
+        Gusano gusanito = new Gusano();
+        gusanito.setVisible(true);
+    }
+    
+}
